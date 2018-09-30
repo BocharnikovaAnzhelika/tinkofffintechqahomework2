3e42a26114abe9db3d50554ecb9e19b6e067a6e1
Anzhelika Bocharnikova <bocharnikovaanzhelika@gmail.com>
2018-09-30 22:15:55 +0300

    Добавлен метод, который создает папку с рандомно сгенерированным именем

---
+++ src/test/java/CreateFolderWithEmptyTxt.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class CreateFolderWithEmptyTxt {
+    public static void main(String[] args) throws IOException {
+        FolderWithEmptyTxt folderWithEmptyTxt = new FolderWithEmptyTxt();
+        folderWithEmptyTxt.Create();
+    }
+}
